8a3462c1966d9db43fed0e342d862f5bf5a0b0a7
TheBalthazar19 <rishavshekhar19@gmail.com>
2025-04-03 12:03:13 +0530

    add initial Docker configuration and server setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/ping', (c) => {
+  return c.json({ pong: 'Hello, World' })
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+export default app
